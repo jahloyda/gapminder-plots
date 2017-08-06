df09f673b54a85d418e385b2aed29f2a0b116381
Jamie Ahloy Dallaire <jahloyda@stanford.edu>
2017-08-06 09:59:18 -0700

    make plot of life expectancy vs per capita gdp

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -1,3 +1,10 @@
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv",
               "data/gapminder-FiveYears.csv")
 gapminder <- read.csv("data/gapminder-FiveYears.csv")
+
+library(ggplot2)
+
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp, color = continent)) +
+  theme_bw() +
+  geom_point() +
+  scale_x_log10()
